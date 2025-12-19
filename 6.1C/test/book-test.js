fe6cd80c65960658f1eae33a80ee1ba3a49a892b
mbkskarthik-beep <mbkskarthik@gmail.com>
2025-12-19 17:48:39 +1100

    Adding 6.1C

---
+++ 6.1C/test/book-test.js
@@ -0,0 +1,34 @@
+const request = require("supertest");
+const { expect } = require("chai");
+const app = require("../app");
+
+// Simple calculation function for testing
+const countBooks = (books) => books.length;
+
+describe("Book API Tests", () => {
+
+  it("GET /books should return an array", async () => {
+    const res = await request(app).get("/books");
+    expect(res.status).to.equal(200);
+    expect(res.body).to.be.an("array");
+  });
+
+  it("GET /books/:id should return 404 for unknown book", async () => {
+    const res = await request(app).get("/books/invalid-id");
+    expect(res.status).to.equal(404);
+  });
+
+});
+
+describe("Calculation Function Tests", () => {
+
+  it("should correctly count number of books", () => {
+    const books = [{}, {}, {}];
+    expect(countBooks(books)).to.equal(3);
+  });
+
+  it("should return 0 when book list is empty", () => {
+    expect(countBooks([])).to.equal(0);
+  });
+
+});
