d4853fcc17529955b397c21eeffa8d83cace3cc1
mbkskarthik-beep <mbkskarthik@gmail.com>
2026-01-05 12:09:10 +1100

    modified 4.2P

--- 4.2P/seed.js
+++ 4.2P/seed.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
-const User = require("../server");   
+const User = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/myprojectDB");
+
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB for seeding!");
 });
 
 const sampleUser = new User({
   username: "jack",
-  email: "mbkskarthik",
+  email: "mbkskarthik@gmail.com",
   age: 28,
   isActive: true,
   isVerified: false,
@@ -21,7 +22,7 @@ sampleUser
     console.log("Sample user saved!");
     mongoose.connection.close();
   })
-  .catch((err) => {
-    console.error(err);
+  .finally(() => {
     mongoose.connection.close();
   });
+
